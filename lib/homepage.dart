5e09219cf9db96b2b020f51f9e69aa300dadf0f2
Jakariya (হাদি) <jhalhadi@gmail.com>
2021-12-22 12:03:49 +0600

    logica implementd

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -21,6 +21,10 @@ class _HomePageState extends State<HomePage> {
   ];
 
   int index = 0;
+  Color listTileBgColorA = Colors.white;
+  Color listTileBgColorB = Colors.white;
+  Color listTileBgColorC = Colors.white;
+  Color listTileBgColorD = Colors.white;
 
   @override
   Widget build(BuildContext context) {
@@ -61,45 +65,141 @@ class _HomePageState extends State<HomePage> {
                   fontWeight: FontWeight.w500,
                   color: Colors.white,
                 )),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(onPressed: (){
-                if(index < 4){
-                  index++;
-                }else{
-                  index=0;
-                }
-                setState(() {
-
-                });
-              }, child: const Text('Next', style: TextStyle(color: Colors.white),)),
-            ),
             const SizedBox(
               height: 16,
             ),
              BuildListTile(
+               listTileBgColor: listTileBgColorA,
               serial: 'A',
               option: questions[index].opt1,
               trail: Icon(null),
+               onOptionPress: (){
+                if(questions[index].ans == 'a'){
+                  listTileBgColorA = Colors.green;
+                  listTileBgColorB = Colors.white;
+                  listTileBgColorC = Colors.white;
+                  listTileBgColorD = Colors.white;
+                  print('right ans');
+
+
+                }else{
+                  listTileBgColorA = Colors.red;
+                  listTileBgColorB = Colors.white;
+                  listTileBgColorC = Colors.white;
+                  listTileBgColorD = Colors.white;
+                }
+                setState(() {
+
+                });
+               },
             ),
             const SizedBox(height: 16,),
              BuildListTile(
+               listTileBgColor: listTileBgColorB,
               serial: 'B',
               option: questions[index].opt2,
-              trail: Icon(null),
+              trail: Icon(null), onOptionPress: () {
+               if(questions[index].ans == 'b'){
+                 listTileBgColorB = Colors.green;
+                 listTileBgColorA = Colors.white;
+                 listTileBgColorC = Colors.white;
+                 listTileBgColorD = Colors.white;
+                 print('right ans');
+
+
+               }else{
+                 listTileBgColorB = Colors.red;
+                 listTileBgColorA = Colors.white;
+                 listTileBgColorC = Colors.white;
+                 listTileBgColorD = Colors.white;
+               }
+               setState(() {
+
+               });
+             },
             ),
             const SizedBox(height: 16,),
              BuildListTile(
+               listTileBgColor: listTileBgColorC,
               serial: 'C',
               option: questions[index].opt3,
-              trail: Icon(null),
+              trail: Icon(null), onOptionPress: () {
+               if(questions[index].ans == 'c'){
+                 listTileBgColorC = Colors.green;
+                 listTileBgColorB = Colors.white;
+                 listTileBgColorA = Colors.white;
+                 listTileBgColorD = Colors.white;
+                 print('right ans');
+
+
+               }else{
+                 listTileBgColorC = Colors.red;
+                 listTileBgColorB = Colors.white;
+                 listTileBgColorA = Colors.white;
+                 listTileBgColorD = Colors.white;
+               }
+               setState(() {
+
+               });
+             },
             ),
             const SizedBox(height: 16,),
              BuildListTile(
+               listTileBgColor: listTileBgColorD,
               serial: 'D',
               option: questions[index].op4,
-              trail: Icon(null),
+              trail: Icon(null), onOptionPress: () {
+               if(questions[index].ans == 'd'){
+                 listTileBgColorD = Colors.green;
+                 listTileBgColorB = Colors.white;
+                 listTileBgColorC = Colors.white;
+                 listTileBgColorA = Colors.white;
+                 print('right ans');
+
+
+               }else{
+                 listTileBgColorD = Colors.red;
+                 listTileBgColorB = Colors.white;
+                 listTileBgColorC = Colors.white;
+                 listTileBgColorA = Colors.white;
+               }
+               setState(() {
+
+               });
+             },
             ),
+            SizedBox(height: 16,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MaterialButton(
+                  shape: StadiumBorder(),
+                    color: Colors.green,
+                    onPressed: (){
+                  if(index > 0){
+                    index--;
+                  }else{
+                    index=images.length-1;
+                  }
+                  setState(() {
+
+                  });
+                }, child: const Text('Previous', style: TextStyle(color: Colors.white),)),
+                MaterialButton(
+                    shape: StadiumBorder(),
+                    color: Colors.green,
+                    onPressed: (){
+                  if(index < 4){
+                    index++;
+                  }else{
+                    index=0;
+                  }
+                  setState(() {
+
+                  });
+                }, child: const Text('Next', style: TextStyle(color: Colors.white),)),
+              ],
+            )
           ],
         ),
       ),
@@ -112,21 +212,26 @@ class BuildListTile extends StatelessWidget {
     required this.serial,
     required this.option,
     required this.trail,
+    required this.onOptionPress,
+    required this.listTileBgColor,
     Key? key,
   }) : super(key: key);
 
   final String serial;
   final String option;
   final Icon trail;
+  final VoidCallback onOptionPress;
+  final Color listTileBgColor;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.white,
+          color: listTileBgColor,
           border: Border.all(),
           borderRadius: BorderRadius.circular(35)),
       child: ListTile(
+        onTap: onOptionPress,
         leading: CircleAvatar(
           child: Text(serial),
         ),
